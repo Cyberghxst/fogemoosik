1f30fbce921df8d06857759290bb650a23d1554d
Cyberghxst <jeffreyvillatoroquezada@gmail.com>
2024-12-19 13:18:04 -0600

    feat(native): $leaveVoiceChannel

---
+++ src/natives/leaveVoiceChannel.ts
@@ -0,0 +1,18 @@
+import { NativeFunction } from "@tryforge/forgescript"
+import { useQueue } from "discord-player"
+import hasQueue from "@utils/hasQueue"
+
+export default new NativeFunction({
+    name: "$leaveVoiceChannel",
+    version: "1.0.0",
+    description: "Destroys the current voice connection.",
+    brackets: false,
+    unwrap: false,
+    async execute(ctx) {
+        if (hasQueue(ctx)) {
+            await useQueue(ctx.guild).connection.destroy()
+        }
+
+        return this.success()
+    }
+})
